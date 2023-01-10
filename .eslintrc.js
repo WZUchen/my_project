39672121f3a82d5b5a9b2f908d9d3d5b5ce588e3
WZUchen <2311931765@qq.com>
2023-01-10 15:45:06 +0800

    feat: 更新

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,24 @@
+/*
+ * @Description:
+ * @Author: zian_Chen
+ * @Date: 2023-01-10 15:32:52
+ * @FilePath: \my-project\vue_3.2\.eslintrc.js
+ * @LastEditTime: 2023-01-10 15:39:44
+ * @LastEditors: zian_Chen
+ */
 module.exports = {
   root: true,
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/standard'],
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': 0,
+     indent: 0
   }
 }
